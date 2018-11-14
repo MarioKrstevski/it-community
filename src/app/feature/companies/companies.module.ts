43d0c2a1c0f60c0eec64a40d6a962200229e9293
WF | Mario Krstevski <mario.krstevski@webfactory.mk>
2018-11-14 16:02:55 +0100

    ITC-2 Added Other Components
    CompanyCard
    CompanyModal
    CompanyCompact
    Perks
    Map
    Footer

--- src/app/feature/companies/companies.module.ts
+++ src/app/feature/companies/companies.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FiltersComponent } from './components/filters/filters.component';
 import { CompaniesComponent } from './components/companies/companies.component';
 import { CompaniesRoutingModule } from './companies-routing.module';
+import { CompanyCardComponent } from './components/company-card/company-card.component';
+import { CompanyModalComponent } from './components/company-modal/company-modal.component';
+import { CompanyCompactComponent } from './components/company-compact/company-compact.component';
+import { PerksComponent } from './components/perks/perks.component';
+import { MapComponent } from './components/map/map.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-    declarations: [FiltersComponent, CompaniesComponent],
+    declarations: [FiltersComponent, CompaniesComponent, CompanyCardComponent, CompanyModalComponent, CompanyCompactComponent, PerksComponent, MapComponent, FooterComponent],
     imports: [
         CompaniesRoutingModule,
         CommonModule
