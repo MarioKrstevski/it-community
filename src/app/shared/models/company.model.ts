48f61b764d0ded837f5f6cba8caf7d4d58060a10
WF | Mario Krstevski <mario.krstevski@webfactory.mk>
2018-11-14 13:21:34 +0100

    ITC-1 Created the initial company model

---
+++ src/app/shared/models/company.model.ts
@@ -0,0 +1,13 @@
+export class Company {
+  constructor(
+    public name: string,
+    public websiteUrl: string,
+    public description: string,
+    public location: string,
+    public number: string,
+    public companySize: string,
+    public numberOfEmployees: number,
+    public photoUrl: string,
+    public fastResponse: boolean,
+  ) { }
+}
